894f48c1cfded3c2d151093b0c7b42dbdfe476f2
Dong hun <ehdgnswkd5@naver.com>
2022-03-07 17:31:57 +0900

    sss

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: `DongDong.com`,
+  title: `zoomkoding.com`,
   description: `줌코딩의 개발일기`,
   language: `ko`, // `ko`, `en` => currently support versions for Korean and English
   siteUrl: `https://www.zoomkoding.com`,
